d1dc1cd808be3dcb0ee7955bd3ea564c750a6445
Alex Bogdanovski <alex@erudika.com>
2016-06-10 12:49:09 +0300

    deploy.sh tweaks

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ git checkout --orphan gh-pages
 git rebase master
 npm run build.prod
 cp -rf dist/prod/** ./
-git add -A
-git push origin gh-pages
+git add -A && git commit -am "updated angular2-para demo" && git push origin gh-pages
 git checkout master
 echo "-- done --"
