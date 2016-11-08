ede409f1da40efaeba3b28bd8126fc9b7aaa18c8
Alex Bogdanovski <alex@erudika.com>
2016-11-08 15:32:37 +0200

    updated angular2-para

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git add -A && git commit -am "updated angular2-para" && git push origin master
-npm run build.prod
-git checkout gh-pages
-git rebase master
-cp -rf dist/prod/** ./
-git add -A && git commit -am "updated angular2-para demo" && git push -f origin gh-pages
-git checkout master
+#git add -A && git commit -am "updated angular2-para" && git push origin master
+#npm run build.prod
+#git checkout gh-pages
+#git rebase master
+cp -rf dist/prod/** ./docs/
+git add -A && git commit -am "updated angular2-para" && git push -f origin master
+#git checkout master
 echo "-- done --"
