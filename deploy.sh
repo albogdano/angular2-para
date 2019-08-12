191762dea54ad3b5991dc79a51f009929fe68134
Alex Bogdanovski <alex@erudika.com>
2019-08-13 01:24:58 +0300

    updated demo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ng build --prod --base-href /angular2-para
+ng build --prod --base-href /angular2-para/
 git add -A && git commit -am "updated demo" && git push -f origin master
 echo "-- done --"
