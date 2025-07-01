6e04ab3f27bf6088e6c9d36cfe7e2f6c406bcb40
Alex Bogdanovski <alex@erudika.com>
2025-07-01 16:30:34 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,21 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeService } from './recipe.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { CoreModule } from './core/core.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    CoreModule,
-    AboutModule,
-    HomeModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-  ],
-  providers: [RecipeService],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA], imports: [BrowserModule,
+        AppRoutingModule,
+        CoreModule,
+        AboutModule,
+        HomeModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })], providers: [RecipeService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
