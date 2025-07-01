b3cfc7cce804a2d2d40eeed43a66bf2d4e43b417
Alex Bogdanovski <alex@erudika.com>
2025-07-01 20:49:44 +0300

    updated demo

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+rm -rf docs build
 ng build -c production --base-href /angular2-para/
 sed -i -e "s/\"\\/\"/\"\\/angular2-para\\/\"/g" docs/manifest.webmanifest
-
+mv build/browser docs
 git add -A && git commit -am "updated demo" && git push -f origin master
 echo "-- done --"
