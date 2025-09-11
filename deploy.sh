a08f652db59219f4145651d48e62391f49fae58e
Alex Bogdanovski <alex@erudika.com>
2025-09-11 14:11:46 +0300

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ ng build -c production --base-href /angular2-para/
 sed -i -e "s/\"\\/\"/\"\\/angular2-para\\/\"/g" build/browser/manifest.webmanifest
 mv build/browser docs
 git add -A && git commit -am "updated demo" && git push -f origin master
+bash reset.sh
 echo "-- done --"
